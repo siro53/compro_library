e97f9786d2f4760f3af2394f0d5677544fd89d38
siro53 <shusuke.shirotake@me.com>
2021-02-12 13:45:57 +0900

    add BIT test

---
+++ test/staticrangesum.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+#include "../template/template.cpp"
+#include "../data_structure/BIT.hpp"
+
+int main(){
+    int N, Q;
+    cin >> N >> Q;
+    BIT<ll> bit(N);
+    rep(i, N){
+        int a; cin >> a;
+        bit.add(i, a);
+    }
+
+    while(Q--){
+        int l, r;
+        cin >> l >> r;
+        cout << bit.sum(l, r) << "\n";
+    }
+}
