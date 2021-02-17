e71fa762336947ca72ebc7b9f9da1dcdae68dc92
siro53 <shusuke.shirotake@me.com>
2021-02-17 21:33:18 +0900

    add fft test

---
+++ test/yuki0206.test.cpp
@@ -0,0 +1,27 @@
+#include "compro_library/template/template.cpp"
+#include "compro_library/math/fft.hpp"
+
+int L, M, N, Q;
+vector<double> A, B;
+
+int main() {
+    cin >> L >> M >> N;
+
+    A.resize(N+1); B.resize(N+1);
+    REP(i, L) {
+        int x; cin >> x;
+        A[x] += 1.0;
+    }
+    REP(i, M) {
+        int x; cin >> x;
+        B[N - x] += 1.0;
+    }
+
+    auto C = FFT::conv(A, B);
+
+    cin >> Q;
+    REP(v, Q) {
+        ll res = ll(C[N + v] + 0.5);
+        cout << res << "\n";
+    }
+}
