047249e5bd3aa8aeb0171d5758e42ea51e683421
siro53 <shusuke.shirotake@me.com>
2021-08-06 10:40:51 +0900

    add euler_phi

---
+++ math/euler_phi.hpp
@@ -0,0 +1,12 @@
+template<class T>
+T phi(T n) {
+    T ret = n;
+    for(T i = 2; i * i <= n; i++) {
+        if(n % i == 0) {
+            ret -= ret / i;
+            while(n % i == 0) n /= i;
+        }
+    }
+    if(n > 1) ret -= ret / n;
+    return ret;
+}
