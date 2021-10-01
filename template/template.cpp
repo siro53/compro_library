26c914c1b09872167f63f3eef4ae981bf9087596
siro53 <shusuke.shirotake@me.com>
2021-10-01 13:55:05 +0900

    REPマクロを可変引数にした

--- template/template.cpp
+++ template/template.cpp
@@ -53,10 +53,12 @@ struct Setup {
 } __Setup;
 
 using ll = long long;
+#define OVERLOAD3(_1, _2, _3, name, ...) name
 #define ALL(v) (v).begin(), (v).end()
 #define RALL(v) (v).rbegin(), (v).rend()
-#define FOR(i, a, b) for(int i = (a); i < int(b); i++)
-#define REP(i, n) FOR(i, 0, n)
+#define REP1(i, n) for(int i = 0; i < (n); i++)
+#define REP2(i, a, b) for(int i = (a); i < int(b); i++)
+#define REP(...) OVERLOAD3(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
 const int INF = 1 << 30;
 const ll LLINF = 1LL << 60;
 constexpr int MOD = 1000000007;
