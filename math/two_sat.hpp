bd652a21a4a215d0695f9718074715b18a6c3969
siro53 <shusuke.shirotake@me.com>
2023-02-09 15:51:10 +0900

    fix two_sat

--- math/two_sat.hpp
+++ math/two_sat.hpp
@@ -6,7 +6,7 @@ template<class Cost = int>
 class TwoSat {
 public:
     TwoSat() = default;
-    explicit TwoSat(int N): scc(N * 2), ans(N), called_satisfiable(false) {}
+    explicit TwoSat(int N): N(N), scc(N * 2), ans(N), called_satisfiable(false) {}
 
     void add_clause(int i, bool f_i, int j, bool f_j) {
         if(!f_i) i = NOT(i);
@@ -16,7 +16,7 @@ public:
     }
     bool satisfiable() {
         scc.build();
-        for(int i = 0; i < (int)ans.size(); i++) {
+        for(int i = 0; i < N; i++) {
             if(scc[i] == scc[NOT(i)]) return false;
             ans[i] = (scc[i] < scc[NOT(i)]);
         }
@@ -29,6 +29,7 @@ public:
     }
 
 private:
+    int N;
     SCC<Cost> scc;
     std::vector<bool> ans;
     bool called_satisfiable;
