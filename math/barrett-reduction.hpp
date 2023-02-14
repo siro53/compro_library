1974e5b068359d0373ed2ed3c62e21c52d9d2db4
siro53 <shusuke.shirotake@me.com>
2023-02-14 22:54:32 +0900

    add dynamic-modint and modint

--- math/barrett-reduction.hpp
+++ math/barrett-reduction.hpp
@@ -4,29 +4,33 @@
 
 class BarrettReduction {
 public:
-    using u32 = unsigned int;
-    using u64 = unsigned long long;
-
-    explicit BarrettReduction(u32 mod)
-        : m(mod), m_inv((u64)(-1) / m + 1) {}
-    u32 mul(u32 a, u32 b) const {
-        u64 z = (u64)a * b;
-        u64 v =  (u64)(((__uint128_t)z * m_inv) >> 64);
-        u32 result = (u32)(z - v * m);
+    BarrettReduction(): BarrettReduction(1) {}
+    BarrettReduction(unsigned int m)
+        : m(m), m_inv((unsigned long long)(-1) / m + 1) {}
+    unsigned int mul(unsigned int a, unsigned int b) const {
+        unsigned long long z = (unsigned long long)a * b;
+        unsigned long long v =  (unsigned long long)(((__uint128_t)z * m_inv) >> 64);
+        unsigned int result = (unsigned int)(z - v * m);
         if(m <= result) result += m;
         return result;
     }
-    u32 div(u32 a) const {
-        u32 result = (u32)(((__uint128_t)a * m_inv) >> 64);
+    unsigned int quo(unsigned int a) const {
+        unsigned int result = (unsigned int)(((__uint128_t)a * m_inv) >> 64);
         return result;
     }
-    std::pair<u32, u32> div_with_rem(u32 a) const {
-        u64 v =  (u64)(((__uint128_t)a * m_inv) >> 64);
-        u32 r = (u32)((u64)a - v * m);
+    unsigned int rem(unsigned long long a) const {
+        unsigned long long v = (unsigned long long)(((__uint128_t)a * m_inv) >> 64);
+        unsigned int result = (unsigned int)(a - v * m);
+        if(m <= result) result += m;
+        return result;
+    }
+    std::pair<unsigned int, unsigned int> quorem(unsigned int a) const {
+        unsigned long long v =  (unsigned long long)(((__uint128_t)a * m_inv) >> 64);
+        unsigned int r = (unsigned int)((unsigned long long)a - v * m);
         if(m <= r) r += m;
         return {v, r};
     }
 private:
-    u32 m;
-    u64 m_inv;
+    unsigned int m;
+    unsigned long long m_inv;
 };
--- modint/modint.hpp
+++ modint/modint.hpp
@@ -1,28 +1,32 @@
 #pragma once
 
-template <int mod> struct ModInt {
-    int x;
+#include <istream>
+#include <ostream>
+#include <utility>
+
+template <int mod> class ModInt {
+  public:
     ModInt() : x(0) {}
-    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt(long long y)
+        : x(y >= 0 ? y % umod() : (umod() - (-y) % umod()) % umod()) {}
+    unsigned int val() const { return x; }
     ModInt &operator+=(const ModInt &p) {
-        if((x += p.x) >= mod)
-            x -= mod;
+        if((x += p.x) >= umod()) x -= umod();
         return *this;
     }
     ModInt &operator-=(const ModInt &p) {
-        if((x += mod - p.x) >= mod)
-            x -= mod;
+        if((x += umod() - p.x) >= umod()) x -= umod();
         return *this;
     }
     ModInt &operator*=(const ModInt &p) {
-        x = (int)(1LL * x * p.x % mod);
+        x = (unsigned int)(1ULL * x * p.x % umod());
         return *this;
     }
     ModInt &operator/=(const ModInt &p) {
         *this *= p.inv();
         return *this;
     }
-    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator-() const { return ModInt(-(long long)x); }
     ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
     ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
     ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
@@ -30,32 +34,35 @@ template <int mod> struct ModInt {
     bool operator==(const ModInt &p) const { return x == p.x; }
     bool operator!=(const ModInt &p) const { return x != p.x; }
     ModInt inv() const {
-        int a = x, b = mod, u = 1, v = 0, t;
+        long long a = x, b = mod, u = 1, v = 0, t;
         while(b > 0) {
             t = a / b;
-            swap(a -= t * b, b);
-            swap(u -= t * v, v);
+            std::swap(a -= t * b, b);
+            std::swap(u -= t * v, v);
         }
         return ModInt(u);
     }
-    ModInt pow(int64_t n) const {
+    ModInt pow(unsigned long long n) const {
         ModInt ret(1), mul(x);
         while(n > 0) {
-            if(n & 1)
-                ret *= mul;
+            if(n & 1) ret *= mul;
             mul *= mul;
             n >>= 1;
         }
         return ret;
     }
-    friend ostream &operator<<(ostream &os, const ModInt &p) {
+    friend std::ostream &operator<<(std::ostream &os, const ModInt &p) {
         return os << p.x;
     }
-    friend istream &operator>>(istream &is, ModInt &a) {
-        int64_t t;
+    friend std::istream &operator>>(std::istream &is, ModInt &a) {
+        long long t;
         is >> t;
         a = ModInt<mod>(t);
         return (is);
     }
-    static int get_mod() { return mod; }
+    static constexpr int get_mod() { return mod; }
+
+  private:
+    unsigned int x;
+    static constexpr unsigned int umod() { return mod; }
 };
