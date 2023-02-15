e717f7efb0d9889d04f1fbe1ae9edce5a803b8eb
siro53 <shusuke.shirotake@me.com>
2023-02-15 21:59:31 +0900

    fix eratosthenes

--- math/eratosthenes.hpp
+++ math/eratosthenes.hpp
@@ -5,9 +5,9 @@
 std::vector<bool> eratosthenes(int n) {
     std::vector<bool> res(n, true);
     res[0] = res[1] = false;
-    for(int i = 2; i * i <= n; i++) {
+    for(int i = 2; i * i < n; i++) {
         if(!res[i]) continue;
-        for(int j = i + i; j <= n; j++) res[j] = false;
+        for(int j = i + i; j < n; j++) res[j] = false;
     }
     return res;
 }
