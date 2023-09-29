12b3728fe592d4c7943a4f51c5f428edda08303b
siro53 <shusuke.shirotake@me.com>
2023-09-30 04:41:13 +0900

    change compress

--- misc/compress.hpp
+++ misc/compress.hpp
@@ -7,14 +7,18 @@ template <typename T> class Compress {
   public:
     Compress() = default;
     explicit Compress(const std::vector<T> &v) : dat(v) {
-        std::sort(dat.begin(), dat.end());
-        dat.erase(std::unique(dat.begin(), dat.end()), dat.end());
+        build();
     }
+    void push_back(T val) { dat.push_back(val); }
+	void build() {
+		std::sort(dat.begin(), dat.end());
+        dat.erase(std::unique(dat.begin(), dat.end()), dat.end());
+	}
     int get(T val) const {
         int pos = std::lower_bound(dat.begin(), dat.end(), val) - dat.begin();
         return pos;
     }
-    T operator[](const int &i) const { return dat[i]; }
+    T operator[](int i) const { return dat[i]; }
     size_t size() const { return dat.size(); }
 
   private:
