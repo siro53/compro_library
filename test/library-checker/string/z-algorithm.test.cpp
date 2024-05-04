f7521ffa7432dd7ea352ed2902786258881f813b
siro53 <shusuke.shirotake@me.com>
2024-05-04 17:58:48 +0900

    add z-algorithm

---
+++ test/library-checker/string/z-algorithm.test.cpp
@@ -0,0 +1,8 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+#include "../../../template/template.cpp"
+#include "../../../string/z-algo.hpp"
+
+int main() {
+    STRING(S);
+    print(z_algo(S));
+}
